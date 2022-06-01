710d84651239b2ace492ad6c43d7ebd20062e9b9
Sergey Kachalov <sergey.kachalov.02@gmail.com>
2022-06-01 14:11:10 +0700

    Добавлено форматирование списка активных заказов

--- barista.py
+++ barista.py
@@ -12,25 +12,58 @@ orders_list = [
     {'id': 2, 'code': 37465027, 'name': 'Латте', 'status': 'in process', 'time': '14:20'}
 ]
 
+
+def convert_orders_list_to_string(orders_list):
+    orders_string = ''
+
+    # сначала идут новые заказы
+    sorted_orders_list = sorted(orders_list, key=lambda order: order['time'], reverse=True)
+
+    # убираем завершенные заказы
+    filtered_orders_list = [order for order in sorted_orders_list if order['status'] != 'completed']
+
+    # получаем список уникальных кодов
+    orders_by_code = list(set([x['code'] for x in filtered_orders_list]))
+
+    for orders in orders_by_code:
+        # записываем одинаковые заказы в один список
+        same_order = [o for o in filtered_orders_list if o['code'] == orders]
+
+        # достаем список кофе из одного заказа
+        coffee_list = []
+        for order in same_order:
+            coffee_list.append(order['name'])
+
+        # формируем отформатированный список
+        orders_string += f'/{same_order[0]["code"]} | Кофе: {", ".join(coffee_list)} ' \
+                         f'| Время: {same_order[0]["time"]} | Статус: {convert_status(same_order[0]["status"])}\n'
+
+    return orders_string
+
+def convert_status(status):
+    if status == 'new':
+        return 'Новый'
+    elif status == 'in process':
+        return 'В процессе'
+    elif status == 'waiting':
+        return 'Ожидание'
+    else:
+        return 'Завершен'
+
+
 # команда-помощник
 @bot.message_handler(commands=['help'])
 def help(message):
     help_string = 'Это бот для баристы, который позволит Вам работать с заказами.\n\n' \
                   '<strong>Список команд</strong>\n' \
                   '/orders - получить список активных заказов\n' \
-                  '/?  (? - идентификатор заказа) - получить заказ по его идентификатору'
+                  '/?  (? - код заказа) - получить заказ по его коду'
 
     bot.send_message(message.chat.id, help_string, parse_mode='html')
 
 # команда для получения списка заказов
 @bot.message_handler(commands=['orders'])
 def orders(message):
-    new_orders = ''
-
-    for order in orders_list:
-        if order['status'] != 'completed':
-            new_orders += f'/{order["code"]} | Кофе: {order["name"]} | Время: {order["time"]} | Статус: {order["status"]}\n'
-
-    bot.send_message(message.chat.id, new_orders)
+    bot.send_message(message.chat.id, convert_orders_list_to_string(orders_list))
 
 bot.polling(none_stop=True)
