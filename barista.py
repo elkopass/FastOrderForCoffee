b406f177f0ee52e9be04fc356a68bd3d785e1375
Timofey Veretnov <tsuveret@gmail.com>
2022-06-03 04:30:52 +0700

    Добавил интеграцию с бд, а так же работа с ней

---
+++ barista.py
@@ -0,0 +1,140 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5240548361:AAEbvuwJy3-ErEJ3WeepU8zsYOUdw0u3dHw')
+
+orders_list = [
+    {'id': 4, 'code': 73872304, 'name': 'Капучино', 'status': 'new', 'time': '14:30' },
+    {'id': 5, 'code': 73872304, 'name': 'Эспрессо', 'status': 'new', 'time': '14:30'},
+    {'id': 0, 'code': 89374287, 'name': 'Американо', 'status': 'completed', 'time': '14:15'},
+    {'id': 3, 'code': 37593577, 'name': 'Капучино', 'status': 'new', 'time': '14:28'},
+    {'id': 1, 'code': 37465027, 'name': 'Латте', 'status': 'in process', 'time': '14:20'},
+    {'id': 2, 'code': 37465027, 'name': 'Латте', 'status': 'in process', 'time': '14:20'}
+]
+
+
+def convert_orders_list_to_string(orders_list):
+    orders_string = ''
+
+    # сначала идут новые заказы
+    sorted_orders_list = sorted(orders_list, key=lambda order: order['time'], reverse=True)
+
+    # убираем завершенные заказы
+    filtered_orders_list = [order for order in sorted_orders_list if order['status'] != 'completed']
+
+    # получаем список уникальных кодов
+    orders_by_code = list(set([x['code'] for x in filtered_orders_list]))
+
+    for orders in orders_by_code:
+        # записываем одинаковые заказы в один список
+        same_order = [o for o in filtered_orders_list if o['code'] == orders]
+
+        # достаем список названий элементов из одного заказа
+        names_list = []
+        for order in same_order:
+            names_list.append(order['name'])
+
+        # формируем отформатированный список
+        orders_string += f'/{same_order[0]["code"]} | Заказ: {", ".join(names_list)} ' \
+                         f'| Время: {same_order[0]["time"]} | Статус: {convert_status(same_order[0]["status"])}\n'
+
+    return orders_string
+
+def convert_order_to_string(order_code):
+    # получаем список элементов заказа с соответствующим кодом
+    this_items_list = [order for order in orders_list if order["code"] == order_code]
+
+    # если заказа с данным кодом не существует, возвращаем None
+    if len(this_items_list) == 0:
+        return None
+
+    # достаем список названий элементов из заказа
+    names_list = [order["name"] for order in this_items_list]
+
+    order_string = f'Заказ: {", ".join(names_list)} | Время: {this_items_list[0]["time"]} ' \
+                   f'| Статус: {convert_status(this_items_list[0]["status"])}\n'
+
+    return order_string, this_items_list[0]["status"]
+
+def change_orders_status(order_code, status):
+    for order in orders_list:
+        if order["code"] == order_code:
+            order["status"] = status
+
+def convert_status(status):
+    if status == 'new':
+        return 'Новый'
+    elif status == 'in process':
+        return 'В процессе'
+    elif status == 'waiting':
+        return 'Ожидание'
+    else:
+        return 'Завершен'
+
+
+# команда-помощник
+@bot.message_handler(commands=['help'])
+def help(message):
+    help_string = 'Это бот для баристы, который позволит Вам работать с заказами.\n\n' \
+                  '<strong>Список команд</strong>\n' \
+                  '/orders - получить список активных заказов\n' \
+                  '/?  (? - код заказа) - получить заказ по его коду'
+
+    bot.send_message(message.chat.id, help_string, parse_mode='html')
+
+# команда для получения списка заказов
+@bot.message_handler(commands=['orders'])
+def orders(message):
+    bot.send_message(message.chat.id, convert_orders_list_to_string(orders_list))
+
+# команда для полученя заказа по его коду
+@bot.message_handler()
+def order(message):
+    try:
+        order_code = int(message.text[1:])
+        order_string, status = convert_order_to_string(order_code)
+
+        if order_string == None:
+            error_string = f'Заказ с кодом {order_code} не найден'
+            bot.send_message(message.chat.id, error_string)
+        else:
+            keyboard = types.InlineKeyboardMarkup()
+
+            if status == 'new':
+                key = types.InlineKeyboardButton(text='В процессе', callback_data=f'in_process {order_code}')
+            elif status == 'in process':
+                key = types.InlineKeyboardButton(text='Ожидание', callback_data=f'waiting {order_code}')
+            elif status == 'waiting':
+                key = types.InlineKeyboardButton(text='Завершен', callback_data=f'completed {order_code}')
+
+            try:
+                keyboard.add(key)
+                bot.send_message(message.chat.id, convert_order_to_string(order_code), reply_markup=keyboard)
+            except:
+                bot.send_message(message.chat.id, convert_order_to_string(order_code))
+    except:
+        error_string = f'Команда {message.text} не найдена. ' \
+                       f'Обратитесь к /help, чтобы получить список доступных команд'
+        bot.send_message(message.chat.id, error_string)
+
+# функция для обработки кнопок
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    callback_data = call.data.split()
+
+    state = callback_data[0]
+    order_code = int(callback_data[1])
+
+    if state == "in_process":
+        change_orders_status(order_code, 'in process')
+        bot.send_message(call.message.chat.id, f'Заказ /{order_code} в процессе')
+
+    elif state == "waiting":
+        change_orders_status(order_code, 'waiting')
+        bot.send_message(call.message.chat.id, f'Заказ /{order_code} завершен и ждет получателя')
+
+    elif state == "completed":
+        change_orders_status(order_code, 'completed')
+        bot.send_message(call.message.chat.id, f'Заказ /{order_code} получен')
+
+bot.polling(none_stop=True)
--- order.py
+++ order.py
@@ -1,10 +1,104 @@
-
 from array import array
+from audioop import add
+from enum import auto
 from statistics import median
 import struct
+from turtle import pos, width
 import telebot
 from telebot import types
 from datetime import datetime
+import sqlite3
+from sqlite3 import Error
+
+from sqlalchemy import MetaData, Table, String, Integer, Column, Text, DateTime, Boolean, ForeignKey
+from datetime import datetime
+from sqlalchemy import insert, select
+from sqlalchemy import  create_engine
+
+engine = create_engine('sqlite:///sqlite3.db')
+
+engine.connect()
+
+print(engine)
+
+metadata = MetaData()
+users = Table('users', metadata, 
+    Column('id', Integer(), primary_key=True),
+    Column('userId', String(200), nullable=False),
+    Column('roleId', String(200),  nullable=False),
+    )
+
+orders = Table('orders', metadata, 
+    Column('id', Integer(), primary_key=True),
+    Column('userId',  ForeignKey("users.id")),
+    Column('status', String(200), nullable=False),
+    Column('time', String(200),  nullable=False),
+    )
+
+positions = Table('positions', metadata, 
+    Column('id', Integer(), primary_key=True),
+    Column('name', String(200), nullable=False),
+    Column('size', String(200),  nullable=False),
+    )
+
+pos_ord = Table('pos_ord', metadata,
+    Column('pos_id', ForeignKey('positions.id')),
+    Column('ord_id', ForeignKey('orders.id'))
+)
+
+def createDB():
+   
+    
+    metadata.create_all(engine)
+    
+    #1
+    ins = insert(users).values(
+        userId = '2323232',
+        roleId = '1'
+    )
+    conn = engine.connect()
+    r = conn.execute(ins)
+    print(r.inserted_primary_key)
+
+    #2
+    ins = insert(orders).values(
+        userId = '1',
+        status = 'new', 
+        time = '12:30'
+    )
+    conn = engine.connect()
+    r = conn.execute(ins)
+    print(r.inserted_primary_key)
+
+    #3
+    ins = insert(positions).values(
+        name = 'capuccino',
+        size = 'normal'
+    )
+    conn = engine.connect()
+    r = conn.execute(ins)
+    print(r.inserted_primary_key)
+
+    #4
+    ins = insert(positions).values(
+        name = 'americano',
+        size = 'normal'
+    )
+    conn = engine.connect()
+    r = conn.execute(ins)
+    print(r.inserted_primary_key)
+    
+    #5
+    ins = insert(pos_ord).values(
+        pos_id = '1',
+        ord_id = '1'
+    )
+    conn = engine.connect()
+    r = conn.execute(ins)
+    print(r.inserted_primary_key)
+
+##createDB()
+
 
 bot = telebot.TeleBot('5240548361:AAEbvuwJy3-ErEJ3WeepU8zsYOUdw0u3dHw')
 class Order:
@@ -18,24 +112,69 @@ class Order:
     def display(self): 
         print("ID: %d nName: %s" % (self.id, self.code)) 
 
-int1 = Order(2, 5,3 ,3 ,3)
-int1.display()
+    def setPosition(self, position):
+        self.arrayOfPositions.append(position)
+
+
+order = Order(2, 5,[] ,"new" , "00:00")
+order.display()
+def sendOrder(Order):
+    ## send order
+    print("Order is send") 
+
+# старт бота
+@bot.message_handler(commands=['start'])
+def start(message):
+    help_string = 'бот запущен'
+
+    s = select([users]).where(
+    users.c.userId == message.chat.id
+    )
+    conn = engine.connect()
+    r = conn.execute(s)
+    if(r.fetchall()==[]):
+        ins = insert(users).values(
+            userId = message.chat.id,
+            roleId = 'user'
+        )
+        conn = engine.connect()
+        r = conn.execute(ins)
+        print(r.inserted_primary_key)
     
-@bot.message_handler(commands=['menu'])
-def menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
-    item1 = types.KeyboardButton("Кофе")
-    item2 = types.KeyboardButton("Холодные напитки")
-    item3 = types.KeyboardButton("Закуски")
-    item4 = types.KeyboardButton("Сделать заказ")
-    item5 = types.KeyboardButton("Редактировать заказ")
+    item1 = types.KeyboardButton("Посмотреть меню")
+    item2 = types.KeyboardButton("Сделать заказ")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, help_string, parse_mode='html', reply_markup= markup)
 
-    markup.add(item1, item2, item3)
 
-    bot.send_message(message.chat.id, 'Выбери'.format(message.from_user), reply_markup= markup)
+# команда-помощник
+@bot.message_handler(commands=['help'])
+def help(message):
+    help_string = 'Это бот для баристы, который позволит Вам работать с заказами.\n\n' \
+                  '<strong>Список команд</strong>\n' \
+                  '/orders - получить список активных заказов\n' \
+                  '/?  (? - идентификатор заказа) - получить заказ по его идентификатору'
+
+    bot.send_message(message.chat.id, help_string, parse_mode='html')
+
+
+# @bot.message_handler(commands=['menu'])
+# def menu(message):
+#     markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
+#     item1 = types.KeyboardButton("Кофе")
+#     item2 = types.KeyboardButton("Холодные напитки")
+#     item3 = types.KeyboardButton("Закуски")
+#     item4 = types.KeyboardButton("Сделать заказ")
+#     item5 = types.KeyboardButton("Редактировать заказ")
+
+#     markup.add(item1, item2, item3, item4, item5)
+
+#     bot.send_message(message.chat.id, 'Выбери'.format(message.from_user), reply_markup= markup)
     
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
+    cap = 0
     if message.chat.type == 'private':
         if message.text == 'Кофе':
             markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
@@ -46,6 +185,28 @@ def bot_message(message):
             markup.add(item1, item2, back)
 
             bot.send_message(message.chat.id, 'Кофе', reply_markup= markup)
+
+        elif message.text == 'Капучино':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
+            item1 = types.KeyboardButton("Банановый коктейль")
+            back = types.KeyboardButton("Назад")
+
+            markup.add(item1, back)
+
+            bot.send_message(message.chat.id, 'Капучино', reply_markup= markup)
+        
+        elif message.text == 'Экспрессо':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
+            item1 = types.KeyboardButton("Банановый коктейль")
+            back = types.KeyboardButton("Назад")
+
+            markup.add(item1, back)
+
+            bot.send_message(message.chat.id, 'Экспрессо', reply_markup= markup)
+        
+    
+
+
         elif message.text == 'Холодные напитки':
             markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
             item1 = types.KeyboardButton("Банановый коктейль")
@@ -73,5 +234,56 @@ def bot_message(message):
             markup.add(item1, item2, item3)
 
             bot.send_message(message.chat.id, 'Назад', reply_markup= markup)
+        
+        
+        elif message.text == "Посмотреть меню":
+            keyboard = types.InlineKeyboardMarkup()
+            key1 = types.InlineKeyboardButton(text='Капучино 75 рублей', callback_data= "gg")
+            key2 = types.InlineKeyboardButton(text='Американо 110 рублей', callback_data= "gg")
+            key3 = types.InlineKeyboardButton(text='Милкшейк 150 рублей ', callback_data= "gg")
+            key4 = types.InlineKeyboardButton(text='Сэндвич 130 рублей', callback_data= "gg")
+            key5 = types.InlineKeyboardButton(text='Шоколадный Маффин 30 рублей', callback_data= "gg")
+
+            keyboard.add(key1, key2, key3, key4, key5)
+
+            bot.send_message(message.chat.id, 'Посмотреть меню', reply_markup=keyboard)
+            bot.delete_message(message.chat.id, message.id)
+        
+        elif message.text == 'Сделать заказ':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
+            item1 = types.KeyboardButton("Посмотреть заказ")
+            item2 = types.KeyboardButton("Отправить заказ")
+            markup.add(item1, item2)
+
+            keyboard = types.InlineKeyboardMarkup()
+            key1 = types.InlineKeyboardButton(text='Добавить Капучино', callback_data= "gg")
+            key2 = types.InlineKeyboardButton(text='Убрать Капучино', callback_data= "gg")
+            keyboard.add(key1, key2)
+
+            bot.send_message(message.chat.id, 'Сделать заказ', reply_markup=keyboard)
+            
+
+            bot.send_message(message.chat.id, 'Сделать заказ', reply_markup= markup)
+            bot.delete_message(message.chat.id, message.id)
+        elif message.text == 'Добавить капучино':
+            cap += 1
+            bot.send_message(message.chat.id, 'Добавить капучино')
+            bot.delete_message(message.chat.id, message.id)
+
+        elif message.text == 'Удалить  капучино':
+            if(cap < 1):
+               
+                bot.send_message(message.chat.id, 'У вас и так нет капучино в заказе')
+            else:
+                cap -= 1
+                
+                
+            bot.send_message(message.chat.id, 'Удалить капучино')
+            bot.delete_message(message.chat.id, message.id)
+
+        elif message.text == 'Посмотреть заказ':
+            print(cap)
+            bot.send_message(message.chat.id, 'Посмотреть заказ')
+
 
 bot.polling(non_stop= True)
