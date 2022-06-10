06fdb6500125bf8dd04ee36872fe373314172c1e
Sergey Kachalov <sergey.kachalov.02@gmail.com>
2022-06-10 21:01:26 +0700

    Бот сохраняет указанную роль
    Был создан класс user. Если вы указываете правильную роль, бот сохраняет ее id в классе user. После этого в коде будет возможность обращаться к вашей роли с помощью getter'а get_role_id.
    Также если вы изначально укажите неверную роль, бот тут же выводит кнопки для выбора корректной роли (до этого нужно было снова вводить команду /start).

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import telebot
 from telebot import types
 import sqlite3
+from user import user
 
 bot = telebot.TeleBot('5240548361:AAEbvuwJy3-ErEJ3WeepU8zsYOUdw0u3dHw')
 
+your_profile = ()
 
 def convert_role_id_to_string_for_error(role_id):
     if role_id == 2:
@@ -11,12 +13,7 @@ def convert_role_id_to_string_for_error(role_id):
     elif role_id == 1:
         return 'администратором'
 
-
-# стартовая команда
-@bot.message_handler(commands=['start'])
-def start(message):
-    start_string = 'Приветствую! Я бот для быстрого онлайн заказа кофе. Выберите свою роль:'
-
+def chose_role(message, message_text):
     keyboard = types.InlineKeyboardMarkup()
 
     key_admin = types.InlineKeyboardButton(text='Администратор', callback_data='1')
@@ -28,7 +25,14 @@ def start(message):
     key_user = types.InlineKeyboardButton(text='Пользователь', callback_data='3')
     keyboard.add(key_user)
 
-    bot.send_message(message.chat.id, start_string, reply_markup=keyboard)
+    bot.send_message(message.chat.id, message_text, reply_markup=keyboard)
+
+# стартовая команда
+@bot.message_handler(commands=['start'])
+def start(message):
+    start_string = 'Приветствую! Я бот для быстрого онлайн заказа кофе. Выберите свою роль:'
+
+    chose_role(message, start_string)
 
 # функция для обработки кнопок
 @bot.callback_query_handler(func=lambda call: True)
@@ -45,7 +49,14 @@ def callback_worker(call):
     if int(callback_data[0]) < int(your_role):
         error_string = f'Вы не являетесь {convert_role_id_to_string_for_error(int(callback_data[0]))}\n' \
                        f'Выберите правильную роль'
-        bot.send_message(call.message.chat.id, error_string)
+
+        chose_role(call.message, error_string)
+
+    else:
+        your_profile = user(int(callback_data[0]))
+        ok_string= f'Вы выбрали роль <strong>{your_profile.get_role_string()}</strong> \n\n' \
+                    f'Введите команду /help, чтобы обратиться к доступному функионалу'
+        bot.send_message(call.message.chat.id, ok_string, parse_mode='html')
 
 
 bot.polling(none_stop=True)
